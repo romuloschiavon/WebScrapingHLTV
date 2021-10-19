e451a8c9621bf9fbfe591affeae0d63bfe6bcb39
romulo <schiavonromulo@gmail.com>
2021-10-19 09:02:24 -0300

    WebScraping concluido

--- webscraping.py
+++ webscraping.py
@@ -9,7 +9,7 @@ import json
 ############################
 #Instanciando o Opera      #
 ############################
-url = "https://www.hltv.org/stats/teams/maps/8297/furia?startDate=2020-10-14&endDate=2021-10-14&rankingFilter=Top20"
+url = "https://www.hltv.org/stats/teams/map/31/8297/furia?startDate=2020-10-14&endDate=2021-10-14"
 
 option = Options()
 option.headless = True
@@ -24,29 +24,47 @@ time.sleep(0.5) #delay pra nao da merda
 
 #escolher qual o mapa você quer as estatisticas
 driver.find_element_by_xpath(f"//div[@class='stats-top-menu']/div[2]/a[1]").click()
+time.sleep(0.5)
 
-#recuperar os dados
+#Status Crus
+
+rawStats = driver.find_elements_by_xpath(f"//div[@class='stats-row']")
+statTitle = []
+statData = []
+for stat in rawStats:
+    titulo = stat.find_element_by_xpath("span[1]").get_attribute('innerHTML')
+    dado = stat.find_element_by_xpath("span[2]").get_attribute('innerHTML')
+    statTitle.append(titulo)
+    statData.append(dado)
+
+#Status Game Changer
+
+gameChangerStats = driver.find_elements_by_xpath(f"//div[@class='col standard-box big-padding']")
+gameChangerTitle = []
+gameChangerData = []
+for stat in gameChangerStats:
+    titulo = stat.find_element_by_xpath("div[2]").get_attribute('innerHTML')
+    dado = stat.find_element_by_xpath("div[1]").get_attribute('innerHTML')
+    gameChangerTitle.append(titulo)
+    gameChangerData.append(dado)
+
+#Match History
+
 element = driver.find_element_by_xpath(f"//div[@class='stats-section stats-team stats-team-map']")
-conteudo = element.get_attribute('innerHTML')
+conteudo = element.get_attribute('outerHTML')
 
 #começar a parsear o conteudo html para transformar em uma tabela
 soup = BeautifulSoup(conteudo, 'html.parser')
 
 time.sleep(0.5)
 
-rawStats = soup.find_all("div", {"class": "stats-rows"}) #status cru
-gameChanger = soup.find_all("div", {"class": "col standard-box big-padding"}) #status que ganham jogos
 matchHistory = soup.find("table", {"class": "stats-table"}) #historico no mapa
 
 ############################
 #Estruturando o data frame #
 ############################
 
-
 dataFrame = pd.read_html(str(matchHistory))[0]
 dataFrame.columns = ['Data', 'Oponente', 'Evento', 'Resultado']
-
-print(dataFrame)
-
 
 driver.quit()
